355e062c348be3ec9921de2eef391f37b6607d0a
totila6 <totila6@gmail.com>
2025-11-03 13:56:31 +0000

    Create label.js

---
+++ src/components/ui/label.js
@@ -0,0 +1,17 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import { cn } from "../../lib/utils";
+
+const Label = React.forwardRef(({ className, ...props }, ref) => (
+  _jsx("label", {
+    ref: ref,
+    className: cn(
+      "text-sm font-semibold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+      "text-slate-700 mb-1 block",
+      className
+    ),
+    ...props
+  })
+));
+Label.displayName = "Label";
+
+export { Label };
